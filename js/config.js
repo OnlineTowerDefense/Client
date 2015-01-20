efefc04f8a5f365d78e45911c0a83cf9f0dfe72d
BlackScorp <witalimik@web.de>
2015-01-20 19:48:31 +0100

    added config file

---
+++ js/config.js
@@ -0,0 +1,9 @@
+var config = {
+    assets: [
+        {src: 'img/attacker/GRUNT_EAST_NORMAL.png', id: 'grunt'},
+        {src: 'img/attacker/RUNNER_EAST_NORMAL.png', id: 'runner'},
+        {src: 'img/tower/TOWER_BASE.png', id: 'towerBase'},
+        {src: 'img/tower/TOWER_FLAMER.png', id: 'towerFlamer'},
+        {src: 'img/tower/TOWER_GATTLING.png', id: 'towerGattling'},
+    ]
+};
