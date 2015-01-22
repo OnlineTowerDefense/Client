2cdd4696c0edba9e88c48b2476562fc0af4e718f
BlackScorp <witalimik@web.de>
2015-01-22 20:40:52 +0100

    testing running events

--- js/src/Units/Grunt.js
+++ js/src/Units/Grunt.js
@@ -8,24 +8,62 @@ function Grunt(config) {
         name:'object'
     };
     this.speed = 0;
+    this.currentEvent = null;
     this.___init(baseConfig);
 
     function calculateSpeed(startingCoordinate,endingCoordinate,timeDiff){
 
         var distance = Math.Util.distance(startingCoordinate.x,startingCoordinate.y,endingCoordinate.x,endingCoordinate.y);
         self.speed = distance/timeDiff;
-        console.log(self.speed );
+
     }
+
     this.on('MOVETO', function (data) {
+        var targetId = 'unit_'+data.attackerId;
+        if(this.getId() !== targetId){
+            return false;
+        }
+
         if(this.speed == 0){
             var diff = (data.endsAt - data.startsAt)/25;
             calculateSpeed(data.startingCoordinate,data.endingCoordinate,diff)
         }
+        if(this.currentEvent == null){
+
+            this.currentEvent = data;
+            console.log('Added event',data);
+        }
+
+        var deltaX = ( data.endingCoordinate.x - this.getX() );
+        var deltaY = (data.endingCoordinate.y - this.getY());
+
+        //this.setX(deltaX);
+        //this.setY(deltaY);
 
     });
+    this.on('DO_MOVETO',function(data){
+       console.log(this.getId()+ ' is Moving');
 
-    this.on('update',function(){
-        this.setY(this.getY()+this.speed);
+    });
+    this.on('update',function(data){
+
+        if(this.currentEvent != null){
+
+            var time = ~~data.time;
+            var complete = time >= this.currentEvent.endsAt;
+
+            if(complete){
+                console.log('finished event',this.currentEvent,time);
+
+                this.currentEvent = null;
+                return false;
+            }
+
+            if(this.currentEvent != null){
+                this.fire('DO_'+this.currentEvent.type,data,true);
+            }
+        }
+
     });
 
 }
