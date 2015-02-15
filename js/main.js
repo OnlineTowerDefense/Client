6af1adef7cd540d8f7ff860e1cd49050e6a44bb8
Maximilian Höflich <maximilian.hoeflich@googlemail.com>
2015-02-15 20:03:09 +0100

    Client now embeddable into Dungeoncrawler Project

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 $(function(){
 
-    var loader = new Konva.Loader(config.assets);
-    var game = new Game('js/example_data.json','game');
+    var userId = queryParameter('userId');
+    var fightId = queryParameter('fightId');
+
+    var access_token = queryParameter('access_token'); //'2d2779d1-4a73-4a40-bc06-f61de8bc8fea';
+
+    var url = 'js/example_data.json';
+    if( userId && fightId && access_token){
+        url = "http://localhost:8080/api/v1/user/"+userId+"/fight/"+fightId+"/events";
+        $.ajaxSetup({
+            headers: { Authorization: "Bearer "+access_token }
+        });
+    }
+
+    var loader = new Kinetic.Loader(config.assets);
+    var game = new Game(url,'game');
 
     loader.onComplete(function(){
         game.ready = true;
